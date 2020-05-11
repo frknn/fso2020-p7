0c32b8d709f9527c3a9213a93afabb7583697e72
frknn <frknstbs@gmail.com>
2020-05-11 15:01:40 +0300

    part7 exercises completed through 7.7

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -18,7 +18,10 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+    axios.get(url).then(response => setCountry({ data: response.data[0], found: true })).catch(err => setCountry({ found: false }))
+  }, [name])
 
   return country
 }
@@ -40,8 +43,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
